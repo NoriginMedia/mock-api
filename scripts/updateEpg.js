35458163396418399d52b2eb015f57975ccd4f61
Dylan Thomson <4798788+DylanThomson@users.noreply.github.com>
2018-06-19 11:30:17 +0200

    Fix added to resolve epg json path as a package

--- scripts/updateEpg.js
+++ scripts/updateEpg.js
@@ -3,6 +3,13 @@ var moment = require("moment");
 
 var file = "examples/demo/epg.json";
 
+// Todo remove this - Quick fix to allow resolving of module path when run from parent project
+if(__dirname.indexOf("node_modules")) {
+    file = "node_modules/nm-mock-api/" + file;
+}
+
+var epgData = jsonfile.readFileSync(file);
+
 var epgData = jsonfile.readFileSync(file);
 
 var currentDay = moment().format("YYYY-MM-DD");
