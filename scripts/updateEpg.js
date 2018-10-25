907ee39942339c170024855278fab082e1141b16
lucia-gomezrodriguez <41974329+lucia-gomezrodriguez@users.noreply.github.com>
2018-10-25 11:23:18 +0200

    Update scripts/updateEpg.js

--- scripts/updateEpg.js
+++ scripts/updateEpg.js
@@ -4,7 +4,7 @@ var moment = require("moment");
 var file = "examples/demo/epg.json";
 
 // Todo remove this - Quick fix to allow resolving of module path when run from parent project
-if(__dirname.indexOf("node_modules")) {
+if (__dirname.indexOf("node_modules") !== -1) {
     file = "node_modules/nm-mock-api/" + file;
 }
 
@@ -14,40 +14,40 @@ var channels = epgData.channels;
 var currentDay = moment(moment().format("YYYY-MM-DD")).valueOf();
 var nextDay = moment(moment().add(1, "day").format("YYYY-MM-DD")).valueOf();
 
-var addRandomInterval = function(time) {
-  return moment(time).add((Math.floor((Math.random() * 4) + 2)) * 10, 'minutes').valueOf();
+var addRandomInterval = function (time) {
+    return moment(time).add((Math.floor((Math.random() * 4) + 2)) * 10, 'minutes').valueOf();
 };
 
 var programsRotation = [
-  'Supernatural',
-  'Awesome Program',
-  'Game of Thrones',
-  'Cool Stuff',
-  'Vikings',
-  'Interesting Show'
+    'Supernatural',
+    'Awesome Program',
+    'Game of Thrones',
+    'Cool Stuff',
+    'Vikings',
+    'Interesting Show'
 ];
 
 for (var i = 0; i < channels.length; i++) {
-  var schedules = [];
+    var schedules = [];
 
-  var startTime = currentDay;
-  var endTime = addRandomInterval(startTime);
+    var startTime = currentDay;
+    var endTime = addRandomInterval(startTime);
 
-  while(endTime < nextDay) {
-    var program = {};
-    program.title = programsRotation[Math.floor(Math.random() * (programsRotation.length))];
+    while (endTime < nextDay) {
+        var program = {};
+        program.title = programsRotation[Math.floor(Math.random() * (programsRotation.length))];
 
-    program.id = 'dummy_program_id';
-    program.start = moment(startTime).format();
-    program.end = moment(endTime).format();
+        program.id = 'dummy_program_id';
+        program.start = moment(startTime).format();
+        program.end = moment(endTime).format();
 
-    schedules.push(program);
+        schedules.push(program);
 
-    startTime = endTime;
-    endTime = addRandomInterval(endTime);
-  }
+        startTime = endTime;
+        endTime = addRandomInterval(endTime);
+    }
 
-  channels[i].schedules = schedules;
+    channels[i].schedules = schedules;
 }
 
 
