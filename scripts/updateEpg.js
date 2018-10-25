30a0762d855d59ac13b88bac8da4501fec0d15c2
lucia-gomezrodriguez <41974329+lucia-gomezrodriguez@users.noreply.github.com>
2018-10-25 11:25:01 +0200

    Update scripts/updateEpg.js

--- scripts/updateEpg.js
+++ scripts/updateEpg.js
@@ -4,7 +4,7 @@ var moment = require("moment");
 var file = "examples/demo/epg.json";
 
 // Todo remove this - Quick fix to allow resolving of module path when run from parent project
-if (__dirname.indexOf("node_modules") !== -1) {
+if (__dirname.indexOf("node_modules") > -1) {
     file = "node_modules/nm-mock-api/" + file;
 }
 
